d32856a91d0b808ccc3982bbc4317434cdba45fe
Giovanna <gio30amaral@gmail.com>
2021-12-13 20:16:31 -0300

    Implemented unit conversion

--- src/index.js
+++ src/index.js
@@ -22,42 +22,66 @@ let currentDay = document.querySelector("#day");
 currentDay.innerHTML = `${day} ${hours}:${minutes}`;
 
 function showTemperature(response) {
-  document.querySelector(".current-temperature").innerHTML = Math.round(
-    response.data.main.temp
+  let temperatureElement = document.querySelector(".current-temperature");
+  let cityElement = document.querySelector(".city");
+  let descriptionElement = document.querySelector(".forecast");
+  let maxTempElement = document.querySelector(".max-temp");
+  let minTempElement = document.querySelector(".min-temp");
+  let humidityElement = document.querySelector(".humidity");
+  let feelsLikeElement = document.querySelector(".feels-like");
+  let windElement = document.querySelector(".wind-speed");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  humidityElement.innerHTML = response.data.main.humidity;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  document.querySelector(".city").innerHTML = response.data.name;
-  document.querySelector(".forecast").innerHTML =
-    response.data.weather[0].description;
-  document.querySelector(".max-temp").innerHTML = Math.round(
-    response.data.main.temp_max
-  );
-  document.querySelector(".min-temp").innerHTML = Math.round(
-    response.data.main.temp_min
-  );
-  document.querySelector(".humidity").innerHTML = response.data.main.humidity;
-  document.querySelector(".feels-like").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
-  document.querySelector(".wind-speed").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document
-    .querySelector("#icon")
-    .setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#exampleInputEmail1").value;
-
+function search(city) {
   let apiKey = "32130c3b8a0437384dedf304822df8d4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#exampleInputEmail1");
+  search(city.value);
+}
+
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".current-temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".current-temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+search("Rio de Janeiro");
